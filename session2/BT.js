d03045a13047476dead6950bb11518b2d6519e99
Thien-Huong <hoangxuanthienhuong@gmail.com>
2022-12-11 21:28:00 +0700

    Bai Tap B1

--- session2/BT.js
+++ session2/BT.js
@@ -22,8 +22,11 @@ const rs = arr.reduce((result, item) => {
 }, initialArr);
 console.log(rs);
 
-//BT3
 
+
+
+//BT3
+//Cach 1
 const input3 = [1, 2, 3, 5, 6, 4, 2, 1, 6, 3, 5, 3]
 console.log(input3.sort())
 function find (input3) {
@@ -47,38 +50,7 @@ function find (input3) {
 
 find (input3)
 
-// let array = [ 7, 2, 6, 7, 4, 9, 8 ];
-// /*tạo hàm tìm phần tử xuất hiện nhiều nhất trong mảng JavaScript*/
-// function array_unique(array){
-//     array.sort();
-
-//     let max =[0,0];
-
-//     //Sử dụng vòng lặp for để lọc ra các phần tử xuất hiện nhiều hơn 1 lần
-//     //So sánh số lần xuất hiện và thay đổi max khi cần.
-//     let count =1;
-//     for (let i = array.length-1; i > 0; --i) {
-//         if (array[i] == array[i-1]) ++count; //Thấy phần tử trùng nhau thì tiếp tục đếm
-//         else{
-//              //So sánh số lần xuất hiện với max[1] 
-//              if (max[1] < count){ 
-//                   //Nếu tìm thấy phần tử xuất hiện nhiều hơn thì gán phần tử vào max[0]
-//                   //Và gán số lần xuất hiện vào max[1]
-//                   max[0] = array[i];
-//                   max[1] = count;   
-//              }
-//              count = 1;
-//         }
-//     }
-//     console.log( "Phần tử "+max[0]+" xuất hiện nhiều nhất với "+max[1]+" lần");
-// }
-
-
-
-// //Tìm phần tử xuất hiện nhiều nhất trong mảng JavaScript
-// array_unique(array);
-
-
+//Cach 2
 const arr1 = [1, 4, 3, 3, 5, 3, 1, 3, 6];
       let time = 0;
       let item;
